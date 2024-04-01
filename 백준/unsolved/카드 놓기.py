2f73b922034e24574d7b754562fd79e05b2b6d37
김민채 <alsco9707@gmail.com>
2024-04-02 08:47:56 +0900

    .

--- 백준/unsolved/카드 놓기.py
+++ 백준/unsolved/카드 놓기.py
@@ -2,11 +2,19 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
+from collections import deque
 N = int(input())
 arr = list(map(int, input().split()))
-card = deque([i for i in range(1, N+1)])
-result = []
-for i in arr:
-    if i == 3:
-        result.append(card.popleft())
+arr = arr[::-1]
+result = deque([])
 
+for i in range(1, N+1):
+    if arr[i-1] == 1:
+        result.appendleft(i)
+    elif arr[i-1] == 2:
+        result.rotate(-1)
+        result.appendleft(i)
+        result.rotate(1)
+    else:
+        result.append(i)
+print(*result)
