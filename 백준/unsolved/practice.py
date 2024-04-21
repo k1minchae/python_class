03d1b5200cea8cbf1e9cad1152ca104ae53b4010
김민채 <alsco9707@gmail.com>
2024-04-21 19:29:04 +0900

    .

--- 백준/unsolved/practice.py
+++ 백준/unsolved/practice.py
@@ -1,16 +1,27 @@
-# 용액
+# 과제
 import sys
 input = sys.stdin.readline
+from heapq import heappop, heappush
 N = int(input())
-arr = list(map(int, input().split()))
+arr = []
+for _ in range(N):
+    d, w = map(int, input().split())
+    arr.append((d, w))
+dp = [0] * 1001
 arr.sort()
-start = 0
-end = N-1
-min_v = arr[start] + arr[end]
-result = (0, N-1)
-while start < end:
-    val = arr[start] + arr[end]
-    if abs(val) < abs(min_v):
-        result = (start, end)
-        min_v = val
+q = [arr[0][1]]
+d = 2
+for i in range(1, N):
+    day = arr[i][0]
+    score = arr[i][1]
+    if day < d:
+        q_score = heappop(q)
+        if q_score > score:
+            continue
+        else:
+            heappush(q, score)
+    else:
+        heappush(q, score)
+        d += 1
+print(sum(q))
 
