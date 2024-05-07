e449b35eacb4d92e1f8994a5aea53e00bceb228d
k1minchae <alsco9707@gmail.com>
2024-05-07 09:06:30 +0900

    .

--- 백준/practice.py
+++ 백준/practice.py
@@ -1,7 +1,25 @@
-# 월드컵
+# 세 용액
 import sys
-case1 = list(map(int, sys.stdin.readline().split()))
-case2 = list(map(int, sys.stdin.readline().split()))
-case3 = list(map(int, sys.stdin.readline().split()))
-case4 = list(map(int, sys.stdin.readline().split()))
+input = sys.stdin.readline
+N = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
 
+val = float('inf')
+result = (0, 0, 0)
+for i in range(N):
+    start = i+1
+    end = N-1
+    while start < end:
+        v = arr[start] + arr[end] + arr[i]
+        if v == 0:
+            print(arr[i], arr[start], arr[end])
+            exit(0)
+        if abs(v) < abs(val):
+            val = v
+            result = (arr[i], arr[start], arr[end])
+        if v < 0:
+            start += 1
+        else:
+            end -= 1
+print(*result)
